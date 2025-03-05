66fc93ada1bcac852e9c21c3ddb48f2ce5d92aea
Enver Eser <env.er@web.de>
2025-03-05 10:07:47 +0100

    breadth first search algorithm

--- algorithms/search_algorithms.py
+++ algorithms/search_algorithms.py
@@ -17,5 +17,29 @@ class SearchAlgorithms():
 
 
     def breadth_first_search(self, maze, width, height):
-        # Breadth First Search algorithm
-        return
+        from collections import deque
+        queue = deque([(0, 0)])
+        visited = [[False] * width for _ in range(height)]
+        visited[0][0] = True
+        parent = { (0, 0): None }
+
+        while queue:
+            x, y = queue.popleft()
+            if (x, y) == (width - 1, height - 1):
+                break
+
+            directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+            for dx, dy in directions:
+                nx, ny = x + dx, y + dy
+                if 0 <= nx < width and 0 <= ny < height and not visited[ny][nx] and maze[ny][nx] == 1:
+                    queue.append((nx, ny))
+                    visited[ny][nx] = True
+                    parent[(nx, ny)] = (x, y)
+
+        path = []
+        step = (width - 1, height - 1)
+        while step:
+            path.append(step)
+            step = parent[step]
+        path.reverse()
+        return path
